eed3d1b800e815c46a90d8b5be3a405e42e0560f
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2026-01-29 16:07:17 +0900

    feat: add basic header and define todo state and type

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client"
+import { useState } from "react";
+
+interface Todo {
+  task: string;
+  isComplete: boolean;
+}
+
 export default function Home() {
-  return 'Let\'s start!!!!';
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return (
+    <header>
+      <h1 className="text-3xl font-bold">
+        TODO LIST
+      </h1>
+    <p className="text-sm text-gray-500">
+      할 일을 정리하고 완료해보십시다리~
+    </p>
+    </header>
+  );
 }
