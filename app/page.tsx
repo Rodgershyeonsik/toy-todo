6544be5a691f57d830e5bbb28477c8c5710b2abe
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2026-02-03 22:59:51 +0900

    feat: implement static todo list UI with dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ interface Todo {
 
 export default function Home() {
 
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([
+    { task: '아침 먹기', isComplete: false, },
+    { task: '점심 먹기', isComplete: false, },
+    { task: '저녁 먹기', isComplete: false, },
+  ]);
 
   return (
     <div className="min-h-screen flex justify-center">
@@ -17,14 +21,25 @@ export default function Home() {
         <h1 className="text-3xl font-bold">
           TODO LIST
         </h1>
-        <p className="text-sm text-gray-500">
-          할 일을 정리하고 완료해보십시다리~
-        </p>
-      </header>
+        <span className="text-sm text-gray-500">
+          할 일을 정리하고 완료해보십시다리^ㅡ^
+        </span>
+        </header>
 
-      <main>
-      </main>
-
+        <main>
+          <div className="py-5">
+            <ul className="list-none space-y-2">
+              {todos.map((todo) => (
+                <li>
+                  <label className="flex items-center gap-1.5">
+                    <input type='checkbox' checked={todo.isComplete} />
+                    <span className="text-xl">{todo.task}</span>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </main>
       </div>
     </div>
   );
