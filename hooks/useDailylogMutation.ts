71939944e4d4b7e8fcd833c12a5c6b19ee762835
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2026-07-03 23:29:19 +0900

    feat: add useDailylogMutation

---
+++ hooks/useDailylogMutation.ts
@@ -0,0 +1,22 @@
+import { upsertDailyLogAction } from "@/actions/dailyLogActions";
+import useUserStore from "@/store/useUserStore";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useDailyLogMutation = () => {
+  const user = useUserStore((state) => state.user);
+  const queryClient = useQueryClient();
+  const upsertMutation = useMutation({
+    mutationFn: ({
+      todoId,
+      elapsedTime,
+    }: {
+      todoId: string;
+      elapsedTime: number;
+    }) => upsertDailyLogAction(todoId, elapsedTime),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: user ? ["daily-logs", user.id] : ["daily-logs"],
+      });
+    },
+  });
+};
