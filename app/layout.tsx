b9db35397addcaab307e74d291353af2e1b8f409
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2026-07-04 11:57:09 +0900

    chore: add sonner toast library and register Toaster in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalModal from "@/components/GlobalModal";
 import Providers from "./providers";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <GlobalModal />
+          <Toaster />
         </Providers>
       </body>
     </html>
